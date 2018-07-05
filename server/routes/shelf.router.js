606de52f78ba6e62952d9c7a1314d6e0ee290f49
Johnny Packard <packard81@me.com>
2018-07-05 16:10:39 -0500

    updated shelf router

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,6 +6,21 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
+    console.log('/shelf GET route');
+    console.log('is authenticated?', req.isAuthenticated());
+    console.log('user', req.user);
+    
+    if (req.isAuthenticated()) {
+        let queryText = `SELECT * FROM "item";`;
+        pool.query(queryText).then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+    } else {
+        res.sendStatus(403);
+    }
     res.sendStatus(200); // For testing only, can be removed
 });
 
@@ -14,7 +29,16 @@ router.get('/', (req, res) => {
  * Add an item for the logged in user to the shelf
  */
 router.post('/', (req, res) => {
-
+    if (req.isAuthenticated()) {
+        console.log('shelf POST route');
+        console.log(req.body);
+        console.log('is authenticated?', req.isAuthenticated());
+        console.log('user', req.user);
+        let queryText = 'INSERT INTO item ('
+        res.sendStatus(201);
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 
